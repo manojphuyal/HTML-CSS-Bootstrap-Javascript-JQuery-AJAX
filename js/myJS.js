96703cfd4348af929d0ad4d8ee5a26a254a58b2f
Manoj Phuyal <phuyalmanoj259@gmail.com>
2021-06-08 20:08:55 +0545

    1st commit for hetauda city collage students

---
+++ js/myJS.js
@@ -0,0 +1,17 @@
+function ab(){
+    alert("Hello i am clicked.2");
+}
+
+document.getElementById("ds").onmouseover = xyz;
+
+function xyz(){
+    var text = "Makawanpur, Nepal.";
+    document.getElementById("dt").innerHTML=text;
+}
+
+function fName(data){
+ document.getElementById("fullName").innerHTML=data.value;
+}
+function lName(data){
+    
+}
