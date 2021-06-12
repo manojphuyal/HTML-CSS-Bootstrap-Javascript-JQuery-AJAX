d4a4acd0fe963c97eb9bde5f2083d1ea064571ae
Manoj Phuyal <phuyalmanoj259@gmail.com>
2021-06-12 10:23:03 +0545

    second commit

--- js/myJS.js
+++ js/myJS.js
@@ -9,9 +9,77 @@ function xyz(){
     document.getElementById("dt").innerHTML=text;
 }
 
-function fName(data){
- document.getElementById("fullName").innerHTML=data.value;
-}
-function lName(data){
-    
-}
+
+
+$(document).ready(function(){
+    $("#loading").fadeOut(2000,function(){
+        $(".content").fadeIn(1000);
+    });
+
+
+        $("#RegFormVali").validate({
+            rules:{
+                fullname:{
+                    required:true,
+                },
+                phone:{
+                    required:true  
+                },
+                mail:{
+                    required:true  
+                },
+                pass:{
+                    required:true  
+                }
+            },
+            messages:{
+                fullname:{
+                    required:"Full name is required"
+                },
+            }
+        });
+
+
+
+    $(".btn2").keydown(function(){
+           var i = $(this).val();
+           if(event.keyCode==77){
+               $(this).hide();
+           }
+    });
+    $("#btn1").click(function(){    
+        $(this).hide();
+     });
+     $(".hidePTag").click(function(){
+         debugger;
+        $(".hideMe").fadeOut(1000);
+    });
+    $("#fName").keyup(function(){
+        var data2= $(this).val();
+        
+        if(data2.includes(1)==true ||data2.includes(2)==true ||
+        data2.includes(3)==true||data2.includes(4)==true||
+        data2.includes(5)==true||data2.includes(6)==true||
+        data2.includes(7)==true||data2.includes(8)==true||
+        data2.includes(9)==true||data2.includes(0)==true)            
+        {
+             var a= data2.replace(data2,'');
+             $(this).val(a);
+        }
+    });
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
